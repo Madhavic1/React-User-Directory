125439a1b714a83055aa32636609b4568beaba7f
Ganesh Chittella <madhavi.0108@gmail.com>
2020-04-24 21:24:09 -0700

    Updates

---
+++ precache-manifest.691e5c36e4d7aa8b3a82dc20649d2c79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4652fdeae4298b3b4683b1cc5a60f862",
+    "url": "/React-User-Directory/index.html"
+  },
+  {
+    "revision": "fcd383099784c6188881",
+    "url": "/React-User-Directory/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dfc2716dafdfd9024e3e",
+    "url": "/React-User-Directory/static/js/2.fbe55bbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-User-Directory/static/js/2.fbe55bbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcd383099784c6188881",
+    "url": "/React-User-Directory/static/js/main.d355a49b.chunk.js"
+  },
+  {
+    "revision": "408b3e5fc30b33b82b46",
+    "url": "/React-User-Directory/static/js/runtime-main.c7a55df9.js"
+  }
+]);
